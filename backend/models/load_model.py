2b51e6c9289fb25a87864bb43ce0a167dceae093
Graffian <rudrabehera2006@gmail.com>
2025-04-26 16:46:35 +0530

    trained models

--- backend/models/load_model.py
+++ backend/models/load_model.py
@@ -4,3 +4,8 @@ import os
 model_path = os.path.join(os.path.dirname(__file__), "human_recog_model.joblib")
 
 human_recog_model = joblib.load(model_path)
+
+
+model_path_shortage_predict = os.path.join(os.path.dirname(__file__), "shortage_predict_model.joblib")
+
+shortage_predict_model = joblib.load(model_path_shortage_predict)
