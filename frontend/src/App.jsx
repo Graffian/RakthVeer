c7f7f00e41776a8e2808c913cd5f6c06728093a2
Graffian <rudrabehera2006@gmail.com>
2025-04-26 09:05:38 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-
-// import DonorLogin from './components/MainPage/DonorLogin'
+import RakthveerLandingPage from "./components/LandingPage/RakthveerLandingPage"
+import BloodDonationPlatform from "./components/MainPage/BloodDonationPlatform"
+import DonorLogin from './components/DonorLoginPage/DonorLogin'
 import './App.css'
 import OrgLogin from './components/OrgLogin/InputDesign'
-
+import { Route , Routes } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
-      <OrgLogin/>
+      <Routes>
+        <Route path="/" element={<RakthveerLandingPage/>}/>
+        <Route path="/login" element={<DonorLogin/>}/>
+        <Route path="/main" element={<BloodDonationPlatform/>}/>
+      </Routes>
     </>
   )
 }
